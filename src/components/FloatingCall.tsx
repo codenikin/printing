6cd7427227c26b6fec9be468f85349d88a62fe29
codenikin <codenikin@gmail.com>
2025-10-26 23:42:39 +0530

    new

---
+++ src/components/FloatingCall.tsx
@@ -0,0 +1,13 @@
+const FloatingCall = () => {
+  return (
+    <div className="floating-call">
+      <a href="tel:+919655449982" aria-label="Call us">
+        <div className="call-button">
+          <i className="fas fa-phone-alt"></i>
+        </div>
+      </a>
+    </div>
+  );
+};
+
+export default FloatingCall;
